49955a5928a51c205e79c34e169c2864eea0bca2
valentin-49 <55395531+valentin-49@users.noreply.github.com>
2024-02-23 14:00:06 +0100

    css correctif

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Action from "./pages/Action";
+import Crypto from "./pages/Crypto";
+import Epargne from "./pages/Epargne";
+import Immobilier from "./pages/Immobilier";
+import AuthPage from "./pages/AuthPage";
 
-import AuthPage from './pages/AuthPage.js';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <AuthPage />
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Routes>
+            <Route path="authentification" element={<AuthPage />} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/action" element={<Action />} />
+            <Route path="/crypto" element={<Crypto />} />
+            <Route path="/epargne" element={<Epargne />} />
+            <Route path="/immobilier" element={<Immobilier />} />
+            <Route path="/compte" element={<Dashboard />} />
+            <Route path="/logout" element={<Dashboard />} />
+        </Routes>
+    );
 }
