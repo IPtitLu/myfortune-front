f989860c4c58cb82935c81ac0790dd661f473ccb
BigBOii <d.barlow@laposte.net>
2024-03-08 15:31:44 +0100

    fixed table csss :fu: :fu: :fu: :fu:

--- src/components/ActifTable.js
+++ src/components/ActifTable.js
@@ -2,12 +2,25 @@ import React from "react";
 
 const ActifTable = (actifs) => {
   return (
-    <div>
-      <div className="relative overflow-x-auto">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <div class="my-8">
+      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Actif
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Montant
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
           <tbody>
             {actifs.actifs.map((actif) => (
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                 <th
                   scope="row"
                   className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
@@ -17,6 +30,12 @@ const ActifTable = (actifs) => {
                 <td className="px-6 py-4">{actif.montantsParDate.map((actif) => (
                     actif.montant+'€'
                 ))}</td>
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  <a href="#" class="text-indigo-600 hover:text-indigo-900">Supprimer</a>
+                </th>
               </tr>
             ))}     
           </tbody>
